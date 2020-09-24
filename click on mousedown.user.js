3560d85b96ad790eed823a167e3e78482c28f148
Kevin Yin <ad8e@users.noreply.github.com>
2020-09-24 17:22:31 -0400

    Update click on mousedown.user.js

--- click on mousedown.user.js
+++ click on mousedown.user.js
@@ -1,25 +1,37 @@
 // ==UserScript==
-// @name	Click on mousedown
-// @version	1
-// @grant	none
+// @name     Click on mousedown
+// @version  1
+// @grant    none
+// @match      *://*/*
 // ==/UserScript==
 
-var lastClick = 0;
+var preventClick = 0;
 window.addEventListener("click", function(e) {
-	if (Date.now() - lastClick < 200) {
+	if (preventClick) {
 		e.preventDefault();
 		e.stopImmediatePropagation();
+		preventClick = 0;
 	}
 }, true);
 
 window.addEventListener("mousedown", function(e) {
+	preventClick = 0;
 	if (e.button !== 0) return;
-	
+
 	if (e.ctrlKey) {
 		//window.open(e.target.href); //often just fails. the href is not always set, sometimes other functions open the URL
 		//window.focus(); //doesn't work. attempts to keep the focus on this page instead of focusing on the newly created page
 		return;
 	}
-	e.target.click();
-	lastClick = Date.now();
+
+	var targ = e.target;
+	if ((targ.tagName !== "a") && (!targ.hasAttribute("href")) && (!targ.hasAttribute("onclick"))) {
+		//sometimes, the target is something like an image inside an <a>
+		targ = e.target.parentElement;
+		if ((targ.tagName !== "a") && (!targ.hasAttribute("href")) && (!targ.hasAttribute("onclick")))
+			return;
+	}
+
+	targ.click();
+	preventClick = 1;
 });
